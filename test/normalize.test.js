afd6a8f8ccbd30e9f0cdda52455ec92be7e401c4
Pablo Terradillos <tehsis@gmail.com>
2015-03-07 20:55:14 -0300

    added tests

---
+++ test/normalize.test.js
@@ -0,0 +1,15 @@
+var characters_map = require('../charmap.json');
+var special_characters = Object.keys(characters_map);
+var characters_array = []; 
+Object.keys(characters_map).forEach(function(key) {
+ characters_array.push(characters_map[key]);
+});
+
+var assert = require('assert');
+console.time('duration');
+var normalize = require('../normalize');
+special_characters.forEach(function(value, index) {
+  var special_char = String.fromCharCode(value);
+  assert(normalize(special_char) === characters_array[index], 'The character ' + special_char + ' was normalized');
+});
+console.timeEnd('duration');
