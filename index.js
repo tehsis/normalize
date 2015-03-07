001c7a6327d3d3f93e57c0062a31067e6450e5c9
Pablo Terradillos <tehsis@gmail.com>
2015-03-07 12:14:17 -0300

    Eliminate innecesary variables and functions

--- index.js
+++ index.js
@@ -6,22 +6,18 @@
   } else if (typeof module !== 'undefined' && module.exports) {
       module.exports = factory(global, global.document);
   } else {
-      global.standarize = factory(global, global.document);
+      global.normalize = factory(global, global.document);
   }
 } (typeof window !== 'undefined' ? window : this, function (window, document) {
   var charmap = require('./charmap');
 
-  var regexstr = '[' + Object.keys(charmap).join(' ') + ']';
-  var regex = new RegExp(regexstr);
+  var regex = new RegExp('[' + Object.keys(charmap).join(' ') + ']');
 
-
-  function replacer(charToReplace) {
-    return charmap[charToReplace];
+  function normalize(str) {
+    return str.replace(regex, function() {
+      return charmap[charToReplace];
+    });
   }
 
-  function standarize(str) {
-    return str.replace(regex, replacer);
-  }
-
-  return standarize;
+  return normalize;
 }));
