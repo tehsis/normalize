c87c487f8ea3568bdc2a5d2410ca722d4a73a0c1
Pablo Terradillos <tehsis@gmail.com>
2015-03-08 19:32:10 -0300

    Improved scrapping script to avoid adding ascii chars to charmap

--- util/getCharMap.js
+++ util/getCharMap.js
@@ -14,7 +14,13 @@ request.get('http://unicode.org/charts/charindex.html')
    var character_title = $(this).parent().prev().text();
    var character = character_title.charAt(0);
 
-   if(character_title.charAt(1) === ' ' || character_title.charAt(1) === ',') {
+   // Check if the title is describing a character
+   var isACharacter = character_title.charAt(1) === ' ' || character_title.charAt(1) === ',';
+  
+   // check if the character is already a standard ascii char
+   var isAscii = code.toUpperCase().charCodeAt(0) >= 65 && code.toUpperCase().charCodeAt(0) <= 90;
+
+   if(isACharacter && !isAscii) {
     map[code.toUpperCase().charCodeAt(0)] = character.toUpperCase();
     map[(code.toLowerCase()).charCodeAt(0)] = character.toLowerCase();
    }
