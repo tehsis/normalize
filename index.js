3e37f74a118b74d228600c7f2cfa1025a7314d96
Daithí Coombes <dcoombes@futurefinance.com>
2016-06-08 11:37:49 +0100

    inject custom charmaps

--- index.js
+++ index.js
@@ -7,13 +7,14 @@
     module.exports = factory(global, global.document);
   } else {
       global.normalize = factory(global, global.document);
-  } 
+  }
 } (typeof window !== 'undefined' ? window : this, function (window, document) {
   var charmap = require('./charmap');
 
   var regex = new RegExp('[' + Object.keys(charmap).map(function(code) {return String.fromCharCode(code); }).join(' ') + ']', 'g');
 
-  function normalize(str) {
+  function normalize(str, customMap) {
+    charmap = customMap || charmap;
     return str.replace(regex, function(charToReplace) {
       return charmap[charToReplace.charCodeAt(0)] || charToReplace;
     });
